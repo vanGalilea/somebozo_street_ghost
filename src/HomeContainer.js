5dda715fc9c10eb1bff93e808a671bdcc734d5cf
vanGalilea <z.galili@icloud.com>
2017-07-14 17:25:28 +0200

    React Router install and config

---
+++ src/HomeContainer.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+
+export class HomeContainer extends PureComponent {
+
+  render() {
+
+    return(
+      <div >
+        <h1>Somebozo Home Page</h1>
+      </div>
+    )
+  }
+}
+
+export default HomeContainer
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import store from './store'
+import { Router, Route, IndexRoute } from 'react-router'
+import store, { history } from './store'
+import HomeContainer from './HomeContainer'
+import registerServiceWorker from './registerServiceWorker'
+import App from './App'
+import './index.css'
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={HomeContainer} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
 
-registerServiceWorker();
+registerServiceWorker()
