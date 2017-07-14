d9ee4e6eb1ad473915376f1e52a5a8bedf18ecb1
vanGalilea <z.galili@icloud.com>
2017-07-14 19:54:08 +0200

    nav bar add, loading and current user to redux store

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import muiTheme from './assets/styles/theme'
+import Navigation from './Navigation'
 
 class App extends Component {
   static childContextTypes = {
@@ -16,9 +17,12 @@ class App extends Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
         <div className="App">
+          <Navigation />
           { this.props.children }
         </div>
       </MuiThemeProvider>
     )
   }
 }
+
+export default App
