7a41b106f0a25a0daba178e756534a05053eb587
vanGalilea <z.galili@icloud.com>
2017-08-11 15:09:27 +0200

    added ignored file

---
+++ src/InstagramCred.js
@@ -0,0 +1,3 @@
+const creds = `//users.instush.com/accordion-zoom/?w=1000&size=50&border=6&shadow=true&crowded=false&sl=true&bg_item=ffffff&bg=transparent&lpc=ffffff&hc=e72476&ltc=3f3f3f&user_id=289040872&username=vangalilea&sid=-1&susername=-1&tag=-1&stype=mine&t=999999R3u-gyUKArY6IQAGZL_h-AiQ5K--RyWC4C6QoG7_2kt58gsCv_LCSXv479WWb_F0fFY30iTomAM`
+
+export default creds
