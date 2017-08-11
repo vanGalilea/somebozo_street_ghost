22da313dd1120c0cfac329eb7e6625dda739e8fe
vanGalilea <z.galili@icloud.com>
2017-08-11 18:43:59 +0200

    api adress change

--- src/api/client.js
+++ src/api/client.js
@@ -4,7 +4,7 @@ import hooks from 'feathers-hooks'
 import auth from 'feathers-authentication-client'
 import superagent from 'superagent'
 
-const host = 'https://somebozo-api.herokuapp.com/'
+const host = 'https://somebozo-api.herokuapp.com'
 export const FEATHERS_TOKEN_KEY = 'somebozoB8'
 
 const feathersClient = feathers()
