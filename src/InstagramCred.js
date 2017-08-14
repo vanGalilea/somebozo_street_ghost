632a9ab9ea99b1251d7fd13194c798feaf609373
vanGalilea <z.galili@icloud.com>
2017-08-14 16:26:26 +0200

    ignored

--- src/InstagramCred.js
+++
@@ -1,3 +0,0 @@
-const creds = `//users.instush.com/accordion-zoom/?w=1000&size=50&border=6&shadow=true&crowded=false&sl=true&bg_item=ffffff&bg=transparent&lpc=ffffff&hc=e72476&ltc=3f3f3f&user_id=5371531890&username=somebozo_street_ghost&sid=-1&susername=-1&tag=-1&stype=mine&t=999999xILhgi1W9a9UEb4r8_FTAYkwPKduusLovcmHILROlhKmccW3BkZx5CbHy28Yxkgf3A0UPvgzqrQ`
-
-export default creds
